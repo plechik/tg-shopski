f22edb8d1ade35e512b87b5b76efb920a56d2534
plechik <ulanovskiyvova2019@gmail.com>
2026-05-28 00:39:55 +0300

    config update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'zolikstore.loca.lt' // Разрешаем Vite принимать запросы от твоего localtunnel
+    ]
+    // allowedHosts: True,
+  }
 })
